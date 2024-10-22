2b96198c4ad60edd033ddbaf310c2d831b4a9c2f
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-22 11:00:30 +0530

    Initial Commit

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+app.post("/login", function(req, res){
+
+});
+
+app.post("/signup", function(req, res){
+    
+});
+
+app.post("/purchase_course", function(req, res){
+    
+});
+
+app.get("/get_course", function(req, res){
+    
+});
+
+app.delete("/delete_course", function(req, res){
+    
+});
