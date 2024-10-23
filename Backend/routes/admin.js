0559aeec3cf96b72725d1f5d04804c9e0a1c0f8a
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-23 21:14:11 +0530

    admin signup end point

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -5,10 +5,23 @@ const { adminmodel } = require("../db");
 const adminrouter = Router();
 
 
-adminrouter.post("/signup", function(req, res){
-    res.json({
-        msg: "admin signup endpoint"
-    });
+adminrouter.post("/signup", async function(req, res){
+    const { email, password, firstname, lastname } = req.body;
+  //TODO: adding zod validation
+  //TODO: password hashing
+  //TODO: adding try catch block for the usermodel
+  
+
+  await adminmodel.create({
+    email: email,
+    password: password,
+    firstname: firstname,
+    lastname: lastname,
+  });
+
+  res.json({
+    msg: "signup succeded",
+  });
 });
 
 adminrouter.post("/signin", function(req, res){
